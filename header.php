1818f991cbdef3b309b1a3199570e51d7c9bcb6a
FelipePietro <nuhhgamer@gmail.com>
2025-03-12 11:46:32 -0300

    Segundo commit

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Bootstrap Demo</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    </head>
+    <body>
+
+    <ul class="nav justify-content-center">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Menu 2</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Menu 3</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+  </li>
+</ul>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+#Cabeçalho
+include 'header.php';
+
+echo 'Olá!';
+
+#Rodapé
+include 'footer.php';
